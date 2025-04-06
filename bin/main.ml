89470011494338afdc1614f1345d7e66304efd6f
FelixLuciano <22255332+FelixLuciano@users.noreply.github.com>
2025-04-05 23:49:33 -0300

    feat: group by and aggregate to build and export the report

--- bin/main.ml
+++ bin/main.ml
@@ -1,22 +1,35 @@
 open Order
-(* open OrderItem *)
+open OrderItem
+open Report
 open Mainlib.Dataframe
 open Mainlib.Csvloader
 
-module OrderDataframe = Dataframe(Order) ;;
-(* module OrderItemDataframe = Dataframe(OrderItem) ;; *)
+module OrderDataFrame = DataFrame(Order) ;;
+module OrderItemDataFrame = DataFrame(OrderItem) ;;
+module ReportDataFrame = DataFrame(Report) ;;
 
-let test = OrderDataframe.init () ;;
+let order = OrderDataFrame.init () ;;
+OrderDataFrame.load order (CsvLoader.load "data/order.csv") ;;
+OrderDataFrame.load order (CsvLoader.load "data/order.csv") ;;
 
-OrderDataframe.load test (CsvLoader.load "data/order.csv") ;;
+let orderItems = OrderItemDataFrame.init () ;;
+OrderItemDataFrame.load orderItems (CsvLoader.load "data/order_item.csv") ;;
+OrderItemDataFrame.load orderItems (CsvLoader.load "data/order_item.csv") ;;
 
-Order.print (OrderDataframe.tail test 0) ;;
-Order.print (OrderDataframe.tail test 1) ;;
-Order.print (OrderDataframe.tail test 2) ;;
-Order.print (OrderDataframe.tail test 3) ;;
+let sumPriceAndTaxes orderItems =
+  OrderItemDataFrame.reduce (fun curr acc ->
+    match acc with
+    | price, tax -> price +. curr.price *. float_of_int curr.quantity, tax +. curr.tax *. curr.price *. float_of_int curr.quantity
+  ) (0.0, 0.0) orderItems
+;;
 
-let reduced = OrderDataframe.reduce (fun curr acc -> acc + curr.id) 0 test ;;
+let groupByOrderId id aggregate =
+  let groups = aggregate (OrderItemDataFrame.filter (fun item -> item.order_id == id) orderItems) in
+  match groups with
+  | price, tax -> Report.create id price tax
+;;
 
-Printf.printf "reduced = %d\n" reduced ;;
+let groups = OrderDataFrame.map (fun row -> groupByOrderId row.id sumPriceAndTaxes) order ;;
+let report = ReportDataFrame.constructor groups ;;
 
-OrderDataframe.store test (CsvLoader.store "cocozinho.csv") ;;
+ReportDataFrame.store report (CsvLoader.store "data/report.csv") ;;
