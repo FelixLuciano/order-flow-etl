12e0ddfca8c6f937daeac42093713660bc1cefba
FelixLuciano <22255332+FelixLuciano@users.noreply.github.com>
2025-04-05 23:54:32 -0300

    refactor: extract function buildReport

--- bin/main.ml
+++ bin/main.ml
@@ -8,28 +8,31 @@ module OrderDataFrame = DataFrame(Order) ;;
 module OrderItemDataFrame = DataFrame(OrderItem) ;;
 module ReportDataFrame = DataFrame(Report) ;;
 
+let buildReport order items =
+  let sumPriceAndTaxes orderItems =
+    OrderItemDataFrame.reduce (fun curr acc ->
+      match acc with
+      | price, tax -> price +. curr.price *. float_of_int curr.quantity, tax +. curr.tax *. curr.price *. float_of_int curr.quantity
+    ) (0.0, 0.0) orderItems
+  in
+  let groupByOrderId id aggregate =
+    let groups = aggregate (OrderItemDataFrame.filter (fun item -> item.order_id == id) items) in
+    match groups with
+    | price, tax -> Report.create id price tax in
+  let groups = OrderDataFrame.map (fun row -> groupByOrderId row.id sumPriceAndTaxes) order in
+  ReportDataFrame.constructor groups
+;;
+
 let order = OrderDataFrame.init () ;;
 OrderDataFrame.load order (CsvLoader.load "data/order.csv") ;;
-OrderDataFrame.load order (CsvLoader.load "data/order.csv") ;;
+(* OrderDataFrame.load order (CsvLoader.load "data/aditional-order.csv") ;; *)
+(* OrderDataFrame.load order (CsvLoader.load "data/aditional-order.csv") ;; *)
 
 let orderItems = OrderItemDataFrame.init () ;;
 OrderItemDataFrame.load orderItems (CsvLoader.load "data/order_item.csv") ;;
-OrderItemDataFrame.load orderItems (CsvLoader.load "data/order_item.csv") ;;
+(* OrderItemDataFrame.load orderItems (CsvLoader.load "data/aditional-order_item.csv") ;; *)
+(* OrderItemDataFrame.load orderItems (CsvLoader.load "data/aditional-order_item.csv") ;; *)
 
-let sumPriceAndTaxes orderItems =
-  OrderItemDataFrame.reduce (fun curr acc ->
-    match acc with
-    | price, tax -> price +. curr.price *. float_of_int curr.quantity, tax +. curr.tax *. curr.price *. float_of_int curr.quantity
-  ) (0.0, 0.0) orderItems
+let report = buildReport order orderItems in
+ReportDataFrame.store report (CsvLoader.store "data/report.csv")
 ;;
-
-let groupByOrderId id aggregate =
-  let groups = aggregate (OrderItemDataFrame.filter (fun item -> item.order_id == id) orderItems) in
-  match groups with
-  | price, tax -> Report.create id price tax
-;;
-
-let groups = OrderDataFrame.map (fun row -> groupByOrderId row.id sumPriceAndTaxes) order ;;
-let report = ReportDataFrame.constructor groups ;;
-
-ReportDataFrame.store report (CsvLoader.store "data/report.csv") ;;
